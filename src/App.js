40c9863cf469ee664d846cfeeb9059b70954b5d7
Jhinaoui Amira <jhinaouiamira306@gmail.com>
2022-01-05 18:01:52 +0100

    modifier le style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import myImage from "./srcImage.png"
+import "./App.css";
+import myImage from "./srcImage.png";
+import "./style.css";
+
 function App() {
   return (
     <div className="App">
-     <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
 
-<h1 className="title red">Your name here</h1>
+        <br/>
 
-<br />
+        <img src={myImage} />
 
-<img src={myImage} />
+        <br />
 
-<br />
+        <img src="/publicImage.jpg" />
+      </div>
 
-<img src="/publicImage.jpg" />
-
-</div>
-
-<video width="320" height="240" controls>
-
-<source src="myVideo.mp4" type="video/mp4" />
-
-</video>
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
